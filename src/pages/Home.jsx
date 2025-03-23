e4035d0f83ed4d5260e9ed13123351f5de555dce
Chetan <56792880+Chetan941@users.noreply.github.com>
2025-03-23 13:07:04 +0530

    added files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+return (
+    <div>
+        <h1 align="center">Welcome to Zomato!</h1>
+        <img src="./images.jpg" alt="" />
+    </div>
+)
+}
+
+export default Home
